ddef78d383421d5d3a0b82bb5c9a98c1644be518
Max Lapan <mlapan@ripe.net>
2016-05-09 09:59:35 +0200

    Increase report

--- q3/q3_train.py
+++ q3/q3_train.py
@@ -9,7 +9,7 @@ import numpy as np
 import tensorflow as tf
 
 BATCH_SIZE = 2000
-REPORT_ITERS = 100
+REPORT_ITERS = 1000
 SAVE_MODEL_ITERS = 1000
 
 # If we did at least 10000 iterations since last sync or average loss fall below threshold we do sync.
@@ -72,7 +72,7 @@ def check_options(loader, replay_buffer):
 
 if __name__ == "__main__":
     LEARNING_RATE = 1e-4
-    TEST_NAME = "t41r3"
+    TEST_NAME = "t41r4"
     TEST_DESCRIPTION = "300k, strange spikes"
     RESTORE_MODEL = "models/model_t41r2-268000"
     GAMMA = 0.99
